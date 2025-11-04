afaa089eb0713d73c8065dc2d0570a04f234d0ed
unknown <juan.lcruz@labinfo.is.escuelaing.edu.co>
2025-11-04 17:20:04 -0500

    feat: Se añade la nueva página para el front

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./Pages/LoginPage/login";
+import Dashboard from "./Pages/Dashboard/dashboard";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
